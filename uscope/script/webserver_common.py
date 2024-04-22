6eb80dca90fc56fd993d825bd46546b3e013dc1e
Simon Wu <swu2208@protonmail.com>
2024-04-22 14:14:02 +0100

    Fix references to app plugin

--- uscope/script/webserver_common.py
+++ uscope/script/webserver_common.py
@@ -6,14 +6,12 @@ from io import BytesIO
 
 import functools
 
-plugin = None
-
 
 def except_wrap(func):
     @functools.wraps(func)
     def wrapper(**kwargs):
         try:
-            plugin.log_verbose(f"{request.url}")
+            current_app.plugin.log_verbose(f"{request.url}")
             return json.dumps(func(**kwargs))
         except Exception as e:
             print(e)
@@ -26,10 +24,11 @@ def except_wrap(func):
 
 
 def make_app(app):
+
     @app.route('/get/position', methods=['GET'])
     @except_wrap
     def position():
-        position = plugin.position()
+        position = current_app.plugin.position()
         return {
             'data': position,
             'status': HTTPStatus.OK,
@@ -46,7 +45,7 @@ def make_app(app):
                                         type=float)
             if this_pos is not None:
                 data[axis] = this_pos
-        plugin.move_absolute(data, block=block)
+        current_app.plugin.move_absolute(data, block=block)
         return {'status': HTTPStatus.OK}
 
     @app.route('/run/move_relative', methods=['GET', 'POST'])
@@ -60,13 +59,13 @@ def make_app(app):
                                         type=float)
             if this_pos is not None:
                 data[axis] = this_pos
-        plugin.move_relative(data, block=block)
+        current_app.plugin.move_relative(data, block=block)
         return {'status': HTTPStatus.OK}
 
     @app.route('/get/system_status', methods=['GET'])
     @except_wrap
     def system_status():
-        system_status = plugin.system_status()
+        system_status = plucurrent_app.pluginin.system_status()
         return {
             'data': system_status,
             'status': HTTPStatus.OK,
@@ -75,7 +74,7 @@ def make_app(app):
     @app.route('/get/is_idle', methods=['GET'])
     @except_wrap
     def is_idle():
-        is_idle = plugin.is_idle()
+        is_idle = current_app.plugin.is_idle()
         return {
             'data': is_idle,
             'status': HTTPStatus.OK,
@@ -84,7 +83,7 @@ def make_app(app):
     @app.route('/get/pyuscope_version', methods=['GET'])
     @except_wrap
     def pyuscope_version():
-        pyuscope_version = plugin.pyuscope_version()
+        pyuscope_version = current_app.plugin.pyuscope_version()
         return {
             'data': pyuscope_version,
             'status': HTTPStatus.OK,
@@ -93,7 +92,7 @@ def make_app(app):
     @app.route('/get/objectives', methods=['GET'])
     @except_wrap
     def objectives():
-        objectives = plugin.get_objectives_config()
+        objectives = current_app.plugin.get_objectives_config()
         return {
             'data': objectives,
             'status': HTTPStatus.OK,
@@ -102,7 +101,7 @@ def make_app(app):
     @app.route('/get/active_objective', methods=['GET'])
     @except_wrap
     def active_objective():
-        objective = plugin.get_active_objective()
+        objective = current_app.plugin.get_active_objective()
         return {
             'data': objective,
             'status': HTTPStatus.OK,
@@ -112,16 +111,16 @@ def make_app(app):
     @except_wrap
     def active_objective_set(objective):
         # Validate objective name before sending request
-        if objective not in plugin.objectives.names():
-            plugin.log(f"WARNING: objective '{objective}' not found")
+        if objective not in current_app.plugin.objectives.names():
+            current_app.plugin.log(f"WARNING: objective '{objective}' not found")
             return {'status': HTTPStatus.BAD_REQUEST}
-        plugin.set_active_objective(objective)
+        current_app.plugin.set_active_objective(objective)
         return {'status': HTTPStatus.OK}
 
     @app.route('/get/imager/imager/get_disp_properties', methods=['GET'])
     @except_wrap
     def imager_get_disp_properties():
-        properties = plugin.imager_get_disp_properties()
+        properties = current_app.plugin.imager_get_disp_properties()
         return {
             'data': properties,
             'status': HTTPStatus.OK,
@@ -130,7 +129,7 @@ def make_app(app):
     @app.route('/get/imager/disp_property/<name>', methods=['GET'])
     @except_wrap
     def imager_get_disp_property(name):
-        val = plugin.imager_get_disp_property(name)
+        val = current_app.plugin.imager_get_disp_property(name)
         return {
             'data': val,
             'status': HTTPStatus.OK,
@@ -144,21 +143,21 @@ def make_app(app):
             # FIXME: not everything is a float
             properties[k] = float(v)
         # Validate objective name before sending request
-        plugin.imager_set_disp_properties(properties)
+        current_app.plugin.imager_set_disp_properties(properties)
         return {'status': HTTPStatus.OK}
 
     @app.route('/run/autofocus', methods=['GET', 'POST'])
     @except_wrap
     def autofocus():
         block = bool(request.args.get("block", default=True, type=int))
-        plugin.autofocus(block=block)
+        current_app.plugin.autofocus(block=block)
         return {'status': HTTPStatus.OK}
 
     @app.route('/get/subsystem_functions', methods=['GET'])
     @except_wrap
     def subsystem_functions():
         return {
-            'data': plugin.subsystem_functions_serialized(),
+            'data': current_app.plugin.subsystem_functions_serialized(),
             'status': HTTPStatus.OK,
         }
 
@@ -168,7 +167,7 @@ def make_app(app):
     def subsystem_function(subsystem, function):
         kwargs = dict(request.args.items())
         print("debug", subsystem, function, kwargs)
-        plugin.subsystem_function_serialized(subsystem_=subsystem,
+        current_app.plugin.subsystem_function_serialized(subsystem_=subsystem,
                                              function_=function,
                                              **kwargs)
         return {'status': HTTPStatus.OK}
@@ -176,7 +175,7 @@ def make_app(app):
     @app.route('/run/wait_imaging_ok', methods=['GET'])
     @except_wrap
     def wait_imaging_ok():
-        plugin.wait_imaging_ok()
+        current_app.plugin.wait_imaging_ok()
         return {
             'status': HTTPStatus.OK,
         }
@@ -188,7 +187,7 @@ def make_app(app):
             request.args.get("wait_imaging_ok", default=True, type=int))
         raw = bool(request.args.get("raw", default=False, type=int))
         format_ = request.args.get("format", default="JPEG", type=str)
-        pil_image = plugin.image(wait_imaging_ok=wait_imaging_ok, raw=raw)
+        pil_image = current_app.plugin.image(wait_imaging_ok=wait_imaging_ok, raw=raw)
         buffered = BytesIO()
         pil_image.save(buffered, format=format_)
         img_str = base64.b64encode(buffered.getvalue()).decode('ascii')
